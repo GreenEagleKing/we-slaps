d9498fdd596c174cd0ae39f5719228de6f70aafe
Ben King <benking1293@gmail.com>
2025-05-10 15:46:08 +0100

    Create svg logo and grid background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import SlapsLogo from "./components/SlapsLogo";
 
-function App() {
-
+export default function App() {
   return (
     <>
-     Hello World
+      {/* Grid background */}
+      <div className="absolute inset-0 -z-10 h-full w-full bg-black bg-[linear-gradient(to_right,rgba(255,255,255,0.3)_2px,transparent_2px),linear-gradient(to_bottom,rgba(255,255,255,0.3)_2px,transparent_2px)] bg-[size:3rem_3rem]"></div>
+      <div className="flex items-center justify-center h-screen">
+        <SlapsLogo />
+      </div>
     </>
-  )
+  );
 }
-
-export default App
