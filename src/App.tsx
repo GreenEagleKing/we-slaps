fac98b571cd23c636339a0dc16d7a08ed79b5aac
Ben King <benking1293@gmail.com>
2025-08-22 16:27:27 +0100

    New design and testing ios PFM graphic

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 // import pfmForWebVideo from "./assets/PFMforWeb.webm";
 import PFMRotateMac from "./assets/PFMRotateOnlyMac.mov";
 import PFMRotateWin from "./assets/PFMRotateOnlyWinWebM.webm";
+import ldnMarathon from "./assets/ldnMarathon2025-105.jpg";
 
 export default function App() {
   const MotionDiv = motion.create("div");
@@ -16,46 +17,55 @@ export default function App() {
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
         >
-          <div
-            className="w-full min-h-screen grid grid-rows-[auto_1fr_auto] grid-cols-1 
-             grid-areas-layout bg-white bg-[linear-gradient(to_right,rgba(128,128,128,0.1)_2px,transparent_2px),linear-gradient(to_bottom,rgba(128,128,128,0.1)_2px,transparent_2px)] bg-[size:2rem_2rem]"
+          <section
+            style={{
+              backgroundImage: `url(${ldnMarathon})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
           >
-            <div className="area-logo flex mt-12 sm:mt-16 justify-center">
-              <SlapsLogo />
+            <div
+              className="w-full min-h-screen grid grid-rows-[auto_1fr_auto] grid-cols-1 
+             grid-areas-layout bg-[linear-gradient(to_right,rgba(255,255,255,0.2)_2px,transparent_2px),linear-gradient(to_bottom,rgba(255,255,255,0.2)_2px,transparent_2px)] bg-[size:2rem_2rem]"
+            >
+              <div className="area-logo flex mt-12 sm:mt-16 justify-center">
+                <SlapsLogo />
+              </div>
+              <div className="area-main flex flex-col items-center justify-center gap-6 sm:gap-8 max-w-xl mx-auto mt-4">
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  className=" sm:mb-10 w-full max-w-lg"
+                >
+                  {/* <source src={PFMRotateWin} type="video/webm" /> */}
+                  <source src={PFMRotateMac} type="video/mp4; codecs=hvc1" />
+                  Your browser does not support the video tag.
+                </video>
+                <p className="uppercase text-center max-w-[300px] sm:max-w-[450px] font-display font-medium italic  text-white text-sm sm:text-base">
+                  This is sun protection for movement.
+                </p>
+                <p className="uppercase text-center max-w-[320px] sm:max-w-[470px] font-display font-medium italic  text-white text-sm sm:text-base">
+                  SLAPS IS FOR OUTDOOR PEOPLE, ADVENTURERS, AND EVERYDAY
+                  ATHLETES ALIKE. THROUGH BETTER DESIGN, REAL STORIES, AND
+                  SHARED EXPERIENCES, WE'RE BRIDGING THE GAP BETWEEN HEALTH AND
+                  SKIN HEALTH.
+                </p>
+                <div className="h-3 sm:h-12"></div>{" "}
+                <p className="font-display font-medium italic uppercase text-center text-white text-sm sm:text-base">
+                  We're working on it...
+                  <br />
+                  Join our mailing list & be the first to know
+                </p>
+                <MailingList />
+              </div>
+              <div className="area-footer pb-10 sm:pb-24 flex justify-center">
+                <Footer />
+              </div>
             </div>
-            <div className="area-main flex flex-col items-center justify-center gap-6 sm:gap-8 max-w-xl mx-auto mt-18">
-              <video
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="mb-14 sm:mb-10 w-full max-w-lg"
-              >
-                <source src={PFMRotateWin} type="video/webm" />
-                <source src={PFMRotateMac} type="video/mp4; codecs=hvc1" />
-                Your browser does not support the video tag.
-              </video>
-              <p className="uppercase text-center max-w-[300px] sm:max-w-[450px] font-display font-medium italic  text-black-slaps text-sm sm:text-base">
-                This is sun protection for movement.
-              </p>
-              <p className="uppercase text-center max-w-[320px] sm:max-w-[470px] font-display font-medium italic  text-black-slaps text-sm sm:text-base">
-                SLAPS IS FOR OUTDOOR PEOPLE, ADVENTURERS, AND EVERYDAY ATHLETES
-                ALIKE. THROUGH BETTER DESIGN, REAL STORIES, AND SHARED
-                EXPERIENCES, WE'RE BRIDGING THE GAP BETWEEN HEALTH AND SKIN
-                HEALTH.
-              </p>
-              <div className="h-3 sm:h-12"></div>{" "}
-              <p className="font-display font-medium italic uppercase text-center text-black-slaps text-sm sm:text-base">
-                We're working on it...
-                <br />
-                Join our mailing list & be the first to know
-              </p>
-              <MailingList />
-            </div>
-            <div className="area-footer pb-10 sm:pb-24 flex justify-center">
-              <Footer />
-            </div>
-          </div>
+          </section>
         </MotionDiv>
       </div>
     </>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,25 +3,25 @@ const Footer = () => {
     <div className="flex flex-col text-center gap-2">
       <a
         href="https://www.instagram.com/we.slaps?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-        className="uppercase underline-offset-4 underline text-black-slaps text-sm sm:text-base"
+        className="uppercase underline-offset-4 underline text-white text-sm sm:text-base"
       >
         Instagram
       </a>
-      <a
+      {/* <a
         href="https://www.linkedin.com/feed/"
-        className="uppercase underline-offset-4 underline  text-black-slaps text-sm sm:text-base"
+        className="uppercase underline-offset-4 underline  text-white text-sm sm:text-base"
       >
         LinkedIn
-      </a>
+      </a> */}
       <a
         href=""
-        className="uppercase underline-offset-4 underline  text-black-slaps text-sm sm:text-base"
+        className="uppercase underline-offset-4 underline  text-white text-sm sm:text-base"
       >
         Contact Us
       </a>
       <a
         href=""
-        className="uppercase underline-offset-4 underline text-black-slaps text-sm sm:text-base"
+        className="uppercase underline-offset-4 underline text-white text-sm sm:text-base"
       >
         LFG
       </a>
