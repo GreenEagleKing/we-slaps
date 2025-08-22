82879280a5a8d29c960c2e4861b27d3b0c237fe6
Ben King <benking1293@gmail.com>
2025-08-22 18:30:13 +0100

    Updated PFM file and new join waiting list style

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { ReactComponent as SlapsLogo } from "./components/SlapsLogo";
 import { motion } from "framer-motion";
 import Footer from "./components/Footer";
 // import pfmForWebVideo from "./assets/PFMforWeb.webm";
-import PFMRotateMac from "./assets/PFMRotateOnlyMacV2.mp4";
-// import PFMRotateWin from "./assets/PFMRotateOnlyWinWebM.webm";
+import PFMRotateMac from "./assets/PFMRotateOnlyMac_hevc_with_alpha.mp4";
+import PFMRotateWin from "./assets/PFMRotateOnlyWinWebM.webm";
 import ldnMarathon from "./assets/ldnMarathon2025-105.jpg";
 
 export default function App() {
@@ -25,16 +25,15 @@ export default function App() {
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
         >
-          <section
-            style={{
-              backgroundImage: `url(${ldnMarathon})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
+          <div className="relative w-full min-h-screen bg-black">
+            <img
+              src={ldnMarathon}
+              alt="London Marathon"
+              className="absolute top-0 left-0 w-full h-full object-cover opacity-85 bg-blend-multiply"
+            />
+
             <div
-              className="w-full min-h-screen grid grid-rows-[auto_1fr_auto] grid-cols-1 
+              className="relative w-full min-h-screen grid grid-rows-[auto_1fr_auto] grid-cols-1 
              grid-areas-layout bg-[linear-gradient(to_right,rgba(255,255,255,0.2)_2px,transparent_2px),linear-gradient(to_bottom,rgba(255,255,255,0.2)_2px,transparent_2px)] bg-[size:2rem_2rem]"
             >
               <div className="area-logo flex mt-12 sm:mt-16 justify-center">
@@ -51,7 +50,7 @@ export default function App() {
                   {isIOS ? (
                     <source src={PFMRotateMac} type="video/mp4; codecs=hvc1" />
                   ) : (
-                    <source src={PFMRotateMac} type="video/webm" />
+                    <source src={PFMRotateWin} type="video/webm" />
                   )}
                   Your browser does not support the video tag.
                 </video>
@@ -64,19 +63,14 @@ export default function App() {
                   SHARED EXPERIENCES, WE'RE BRIDGING THE GAP BETWEEN HEALTH AND
                   SKIN HEALTH.
                 </p>
-                <div className="h-3 sm:h-12"></div>{" "}
-                <p className="font-display font-medium italic uppercase text-center text-white text-sm sm:text-base">
-                  We're working on it...
-                  <br />
-                  Join our mailing list & be the first to know
-                </p>
+                <div className="h-3 sm:h-12"></div>
                 <MailingList />
               </div>
               <div className="area-footer pb-10 sm:pb-24 flex justify-center">
                 <Footer />
               </div>
             </div>
-          </section>
+          </div>
         </MotionDiv>
       </div>
     </>
--- src/components/MailingList.tsx
+++ src/components/MailingList.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { ReactComponent as JoinWaitingList } from "./JoinWaitingList";
 
 const MailingList: React.FC = () => {
   const [mailingSubmissionState, setMailingSubmissionState] = useState<
@@ -72,37 +73,43 @@ const MailingList: React.FC = () => {
   const MotionDiv = motion.create("div");
 
   return (
-    <MotionDiv
-      whileHover={{
-        scale: mailingSubmissionState === "idle" ? 1.05 : getScale(),
-      }}
-      animate={{ scale: getScale() }}
-      transition={{ type: "spring", stiffness: 300, damping: 20 }}
-      className={`mt-4 mb-12 `}
-    >
-      <form onSubmit={submitEmail} className="flex justify-between gap-4">
-        <input
-          className="w-full sm:w-[400px] border border-black-slaps bg-white rounded-sm font-mono text-sm px-6 py-2 flex-grow placeholder:text-black-slaps sm:text-base"
-          placeholder="Enter Email"
-          name="email"
-          type="email"
-          required
-        ></input>
-        <button
-          className={`${submissionStateColour()} transition-colors duration-300 text-black-slaps px-4 py-1 rounded-sm font-mono text-sm border border-black-slaps uppercase sm:text-base`}
-          type="submit"
-          disabled={mailingSubmissionState === "submitting"}
-        >
-          {mailingSubmissionState === "submitting"
-            ? "Submitting..."
-            : mailingSubmissionState === "success"
-            ? "Subscribed!"
-            : mailingSubmissionState === "error"
-            ? "Try again"
-            : "Subscribe"}
-        </button>
-      </form>
-    </MotionDiv>
+    <div className="flex flex-col gap-6">
+      <p className="font-display font-bold italic uppercase text-center text-lime-slaps [text-shadow:1px_0_0_#000,-1px_0_0_#000,0_1px_0_#000,0_-1px_0_#000] text-2xl sm:text-2xl ">
+        We're working on it...
+      </p>
+      <JoinWaitingList />
+      <MotionDiv
+        whileHover={{
+          scale: mailingSubmissionState === "idle" ? 1.05 : getScale(),
+        }}
+        animate={{ scale: getScale() }}
+        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        className={`mb-12 `}
+      >
+        <form onSubmit={submitEmail} className="flex justify-between gap-4">
+          <input
+            className="w-full sm:w-[400px] border border-black-slaps bg-white rounded-sm font-mono text-sm px-6 py-2 flex-grow placeholder:text-black-slaps sm:text-base"
+            placeholder="Enter Email"
+            name="email"
+            type="email"
+            required
+          ></input>
+          <button
+            className={`${submissionStateColour()} transition-colors duration-300 text-black-slaps px-4 py-1 rounded-sm font-mono text-sm border border-black-slaps uppercase sm:text-base`}
+            type="submit"
+            disabled={mailingSubmissionState === "submitting"}
+          >
+            {mailingSubmissionState === "submitting"
+              ? "Submitting..."
+              : mailingSubmissionState === "success"
+              ? "Subscribed!"
+              : mailingSubmissionState === "error"
+              ? "Try again"
+              : "Subscribe"}
+          </button>
+        </form>
+      </MotionDiv>
+    </div>
   );
 };
 
