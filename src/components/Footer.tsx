1051f59d674a7223ac059e233bc9ba2fdd350009
GreenEagleKing <benking1293@gmail.com>
2026-05-21 11:12:01 +0100

    Updated WIP Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as SlapsLogo } from "./SlapsLogo";
+
 interface FooterProps {
   setShowContactModal: React.Dispatch<React.SetStateAction<boolean>>;
   setShowLFGModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,32 +12,44 @@ const Footer = ({
   setShowPrivacyModal,
 }: FooterProps) => {
   return (
-    <div className="flex text-center gap-4 lg:flex-col lg:gap-2">
-      <a
-        href="https://www.instagram.com/we.slaps?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-        className="uppercase underline-offset-4 underline text-white text-sm sm:text-base"
-        target="_blank"
-      >
-        Instagram
-      </a>
-      <a
-        onClick={() => setShowContactModal(true)}
-        className="uppercase underline-offset-4 underline  text-white text-sm sm:text-base cursor-pointer"
-      >
-        Contact Us
-      </a>
-      <a
-        onClick={() => setShowPrivacyModal(true)}
-        className="uppercase underline-offset-4 underline text-white text-sm sm:text-base cursor-pointer"
-      >
-        Privacy
-      </a>
-      <a
-        onClick={() => setShowLFGModal(true)}
-        className="uppercase underline-offset-4 underline text-white text-sm sm:text-base cursor-pointer"
-      >
-        LFG
-      </a>
+    <div className="flex justify-between items-center px-5 py-4">
+      <SlapsLogo className="fill-offwhite-slaps" />
+      <div className="flex items-center gap-8">
+        <a
+          href="https://www.instagram.com/we.slaps?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+          className="uppercase underline-offset-4 underline text-offwhite-slaps text-sm sm:text-base"
+          target="_blank"
+          rel="noreferrer"
+        >
+          About
+        </a>
+        <button
+          onClick={() => setShowLFGModal(true)}
+          className="uppercase underline-offset-4 underline text-offwhite-slaps text-sm sm:text-base cursor-pointer"
+        >
+          Mailing List
+        </button>
+        <a
+          href="https://www.instagram.com/we.slaps?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+          className="uppercase underline-offset-4 underline text-offwhite-slaps text-sm sm:text-base"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+        <button
+          onClick={() => setShowPrivacyModal(true)}
+          className="uppercase underline-offset-4 underline text-offwhite-slaps text-sm sm:text-base cursor-pointer"
+        >
+          Privacy
+        </button>
+        <button
+          onClick={() => setShowContactModal(true)}
+          className="uppercase underline-offset-4 underline text-offwhite-slaps text-sm sm:text-base cursor-pointer"
+        >
+          Contact
+        </button>
+      </div>
     </div>
   );
 };
